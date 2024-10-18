c48ee98536cc41bb83793958e68119339b61b161
Gaurav Kumar Karki <gauravkumarkarki07@gmail.com>
2024-10-19 01:18:14 +1100

    Home page created

---
+++ client/src/Home/views/Index.tsx
@@ -0,0 +1,7 @@
+function Index() {
+  return (
+    <div>This is home</div>
+  )
+}
+
+export default Index
--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,12 +1,27 @@
 import App from "@/App";
-import { createBrowserRouter } from "react-router-dom";
+import AuthenticatedRoutes from "@/Auth/components/AuthenticatedRoutes";
+import { createBrowserRouter, Navigate } from "react-router-dom";
+import Home from "@/Home/views/Index";
 
 export const router=createBrowserRouter([
     {
         path:'',
         element:<App/>,
         children:[
-            
+            {
+                path:'',
+                element:<AuthenticatedRoutes/>,
+                children:[
+                    {
+                        path:'',
+                        element:<Navigate to={'/home'}/>
+                    },
+                    {
+                        path:'home',
+                        element:<Home/>
+                    }
+                ]
+            }
         ]
     }
 ])
