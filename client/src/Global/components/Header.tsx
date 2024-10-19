e01d3e5b7b3eedfde8235329e3812025367342c7
Gaurav Kumar Karki <gauravkumarkarki07@gmail.com>
2024-10-20 00:43:44 +1100

    Chore: Social icons added

--- client/src/Global/components/Header.tsx
+++ client/src/Global/components/Header.tsx
@@ -1,4 +1,8 @@
-import SearchBar from "./SearchBar"
+import SearchBar from "./SearchBar";
+import { FactoryIcon } from "lucide-react";
+import facebookIcon from "../assets/facebook.png";
+import instagramIcon from "../assets/instagram.png";
+import twitterIcon from "../assets/twitter.png";
 
 function Header() {
   return (
@@ -7,15 +11,35 @@ function Header() {
         {/* Upper Part */}
         <section className="border-b px-4 py-1">
           <ul className="flex flex-row justify-between items-center max-w-[1280px] mx-auto">
-            <li className="text-xl">Code Resource</li>
-            <li>Social Links</li>
+            <li className="text-xl text-primary">Code Resource</li>
+            {/* Social Icons */}
+            <section className="flex flex-row gap-4">
+              <li className="w-[20px] h-[20px]">
+                <img
+                  src={facebookIcon}
+                  className="w-full h-full rounded-full overflow-clip object-cover"
+                />
+              </li>
+              <li className="w-[20px] h-[20px]">
+                <img
+                  src={instagramIcon}
+                  className="w-full h-full rounded-full overflow-clip object-contain"
+                />
+              </li>
+              <li className="w-[20px] h-[20px]">
+                <img
+                  src={twitterIcon}
+                  className="w-full h-full rounded-full overflow-clip object-contain"
+                />
+              </li>
+            </section>
           </ul>
         </section>
         {/* Lower Part */}
         <section className="border-b px-4 py-2">
           <ul className="max-w-[1280px] mx-auto flex flex-row justify-between items-center">
             <li>
-              <SearchBar/>
+              <SearchBar />
             </li>
             <section className="flex flex-row gap-4 items-center">
               <li>Home</li>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -12,7 +12,9 @@ export default {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		},
-  		colors: {},
+  		colors: {
+        primary:'#FF4B3E',
+      },
       fontFamily:{
         Montserrat:['Montserrat','sans-serif'],
         Poppins:['Poppins','sans-serif'],
