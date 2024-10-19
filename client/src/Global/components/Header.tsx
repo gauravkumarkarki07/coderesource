382a10888f026a3d9cd0613e582a4f6db9e9aa01
Gaurav Kumar Karki <gauravkumarkarki07@gmail.com>
2024-10-20 00:24:03 +1100

    Fix: Font style to  monst in header

--- client/src/Global/components/Header.tsx
+++ client/src/Global/components/Header.tsx
@@ -2,21 +2,26 @@ import SearchBar from "./SearchBar"
 
 function Header() {
   return (
-    <header className="w-full">
+    <header className="w-full  font-Montserrat font-semibold">
       <nav className="flex flex-col">
         {/* Upper Part */}
-        <section className="border-b px-4 py-4">
-          <ul className="flex flex-row justify-between  max-w-[1280px] mx-auto">
+        <section className="border-b px-4 py-1">
+          <ul className="flex flex-row justify-between items-center max-w-[1280px] mx-auto">
             <li className="text-xl">Code Resource</li>
             <li>Social Links</li>
           </ul>
         </section>
         {/* Lower Part */}
         <section className="border-b px-4 py-2">
-          <ul className="max-w-[1280px] mx-auto">
+          <ul className="max-w-[1280px] mx-auto flex flex-row justify-between items-center">
             <li>
               <SearchBar/>
             </li>
+            <section className="flex flex-row gap-4 items-center">
+              <li>Home</li>
+              <li>Popular</li>
+              <li>More</li>
+            </section>
           </ul>
         </section>
       </nav>
