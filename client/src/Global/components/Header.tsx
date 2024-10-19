ff4b4513ad8ae3cc4e883be2aa76a1fc316fb798
Gaurav Kumar Karki <gauravkumarkarki07@gmail.com>
2024-10-20 00:20:38 +1100

    Feat: Input Field added and Search bar added on header

--- client/src/Global/components/Header.tsx
+++ client/src/Global/components/Header.tsx
@@ -1,21 +1,26 @@
-import { NavLink } from "react-router-dom"
+import SearchBar from "./SearchBar"
 
 function Header() {
   return (
-    <section className="w-full border-b">
-      <nav className="flex justify-between max-w-[1280px] mx-auto px-4 py-4">
-        <ul>
-          <li>CodeResource</li>
-        </ul>
-        <ul>
-          <li>
-            <NavLink to={'/home'}> 
-              Home
-            </NavLink>
-          </li>
-        </ul>
+    <header className="w-full">
+      <nav className="flex flex-col">
+        {/* Upper Part */}
+        <section className="border-b px-4 py-4">
+          <ul className="flex flex-row justify-between  max-w-[1280px] mx-auto">
+            <li className="text-xl">Code Resource</li>
+            <li>Social Links</li>
+          </ul>
+        </section>
+        {/* Lower Part */}
+        <section className="border-b px-4 py-2">
+          <ul className="max-w-[1280px] mx-auto">
+            <li>
+              <SearchBar/>
+            </li>
+          </ul>
+        </section>
       </nav>
-    </section>
+    </header>
   )
 }
 
---
+++ client/src/Global/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+import { Input } from '@/components/ui/input';
+import {SearchIcon} from 'lucide-react';
+
+function SearchBar() {
+  return (
+    <section className='w-[250px] border rounded-lg flex flex-row gap-2 items-center h-[40px]'>
+        <Input 
+        className='w-[200px] focus-visible:outline-none focus-visible:ring-0 border-none shadow-none'
+    placeholder='Search for resources'
+        />
+        <SearchIcon size={20}/>
+    </section>
+  )
+}
+
+export default SearchBar
